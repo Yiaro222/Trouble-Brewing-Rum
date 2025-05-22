707ab4743cf63ff611bdbcde692a1b72df28a39a
Alex <AlexanderCharles@users.noreply.github.com>
2025-05-22 15:56:48 +0100

    Now clears highlights and added icon rendering demo

--- src/main/java/com/RumRunning/TroubleBrewingOverlay.java
+++ src/main/java/com/RumRunning/TroubleBrewingOverlay.java
@@ -1,8 +1,13 @@
 package com.RumRunning;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 import javax.inject.Inject;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 
 import lombok.Setter;
 import net.runelite.api.Point;
@@ -10,47 +15,90 @@ import net.runelite.api.*;
 import net.runelite.api.Client;
 import net.runelite.api.Player;
 import net.runelite.api.TileObject;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.ui.overlay.*;
 import net.runelite.client.ui.overlay.outline.ModelOutlineRenderer;
 import net.runelite.api.events.*;
 
-public class TroubleBrewingOverlay extends Overlay {
-    private final Client client;
-    private final TroubleBrewingPlugin plugin;
-    private final TroubleBrewingConfig config;
-    private final ModelOutlineRenderer modelOutlineRenderer;
-
-    @Setter
-    public GameObject renderableJunglePlant;
-
-    @Inject
-    private TroubleBrewingOverlay(Client client, TroubleBrewingPlugin plugin, TroubleBrewingConfig config, ModelOutlineRenderer modelOutlineRenderer)
-    {
-        this.client = client;
-        this.plugin = plugin;
-        this.config = config;
-        this.modelOutlineRenderer = modelOutlineRenderer;
-        setPosition(OverlayPosition.DYNAMIC);
-        setLayer(OverlayLayer.ABOVE_SCENE);
-    }
-
-    @Override
-    public Dimension render(Graphics2D graphics)
-    {
-        if (renderableJunglePlant != null) {
-            modelOutlineRenderer.drawOutline(renderableJunglePlant, 3, Color.ORANGE, 1);
-            //graphics.draw(renderableJunglePlant.getConvexHull());
-
-        } else {
-            return null;
-        }
-
-        Player player = client.getLocalPlayer();
-        Point test = player.getCanvasTextLocation(graphics, "", player.getLogicalHeight() + 40);
-        modelOutlineRenderer.drawOutline(player, 3, Color.MAGENTA,1);
-        OverlayUtil.renderTextLocation(graphics, test, "Certified Rum Runner", Color.MAGENTA);
 
 
-        return null;
-    }
+@Slf4j
+public class TroubleBrewingOverlay
+extends      Overlay
+{
+	private final Client               client;
+	private final TroubleBrewingPlugin plugin;
+	private final TroubleBrewingConfig config;
+	private final ModelOutlineRenderer modelOutlineRenderer;
+	private final ItemManager          itemManager;
+	
+	@Setter
+	private GameObject renderableJunglePlant;
+	
+	private final BufferedImage tinderboxIcon;
+	
+	
+	@Inject
+	private
+	TroubleBrewingOverlay(Client client,
+	                      TroubleBrewingPlugin plugin,
+	                      TroubleBrewingConfig config,
+	                      ModelOutlineRenderer modelOutlineRenderer,
+	                      ItemManager          itemManager)
+	{
+		this.client               = client;
+		this.plugin               = plugin;
+		this.config               = config;
+		this.modelOutlineRenderer = modelOutlineRenderer;
+		this.itemManager          = itemManager;
+		
+		setPosition(OverlayPosition.DYNAMIC);
+		setLayer(OverlayLayer.ABOVE_SCENE);
+		
+		tinderboxIcon = itemManager.getImage(ItemID.TINDERBOX);
+	 }
+	
+	@Override
+	public Dimension
+	render(Graphics2D graphics)
+	{
+		Player localPlayerData;
+		Point  overheadTextTestPosition;
+		Point  iconTestPosition;
+		
+		if (renderableJunglePlant == null)
+		{
+			log.info("NOTHING TO DRAW");
+			return null;
+		}
+		
+		log.info("DRAWING PLANT");
+		
+		localPlayerData = client.getLocalPlayer();
+		overheadTextTestPosition = localPlayerData.getCanvasTextLocation
+		(
+			graphics, "HeightOffset",
+			localPlayerData.getLogicalHeight() + 40
+		);
+		iconTestPosition = Perspective.getCanvasImageLocation
+		(
+			client,
+			renderableJunglePlant.getLocalLocation(),
+			tinderboxIcon, 150
+		);
+		
+		modelOutlineRenderer.drawOutline(renderableJunglePlant, 3, Color.ORANGE, 1);
+		graphics.draw(renderableJunglePlant.getConvexHull());
+		
+		modelOutlineRenderer.drawOutline(localPlayerData, 3, Color.MAGENTA,1);
+		OverlayUtil.renderTextLocation(graphics, overheadTextTestPosition,
+		                               "Certified Rum Runner", Color.MAGENTA);
+		
+		graphics.drawImage(tinderboxIcon,
+		                   iconTestPosition.getX(), iconTestPosition.getY(),
+		                   null);
+		
+		return null;
+	 }
 }
--- src/main/java/com/RumRunning/TroubleBrewingPlugin.java
+++ src/main/java/com/RumRunning/TroubleBrewingPlugin.java
@@ -12,87 +12,84 @@ import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.api.events.*;
 
+
+
 @Slf4j
 @PluginDescriptor(
 	name = "Trouble Brewing Rum"
 )
-public class TroubleBrewingPlugin extends Plugin
+public class TroubleBrewingPlugin
+extends      Plugin
 {
 	@Inject
-	private Client client;
-
+	private Client                client;
 	@Inject
-	private TroubleBrewingConfig config;
-
+	private TroubleBrewingConfig  config;
 	@Inject
-	private TroubleBrewingOverlay TroubleBrewingOverlay;
-
+	private TroubleBrewingOverlay troubleBrewingOverlay;
 	@Inject
-	private OverlayManager overlayManager;
-
+	private OverlayManager        overlayManager;
+	
+	
+	
 	@Override
-	protected void startUp() throws Exception
+	protected void
+	startUp() throws Exception
 	{
 		log.info(" ##### Plugin started! ##### ");
-		overlayManager.add(TroubleBrewingOverlay);
-		Player player = client.getLocalPlayer();
-		//log.info(player.getName());
-
+		overlayManager.add(troubleBrewingOverlay);
 	}
-
+	
 	@Override
-	protected void shutDown() throws Exception
+	protected void
+	shutDown() throws Exception
 	{
 		log.info(" ##### Plugin stopped! ##### ");
 	}
-
+	
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void
+	onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		if (gameStateChanged.getGameState() == GameState.LOADING)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.StringText1(), null);
+			troubleBrewingOverlay.setRenderableJunglePlant(null);
 		}
 	}
-
-
+	
 	@Subscribe
-	public void onGameObjectSpawned(GameObjectSpawned event)
+	public void
+	onGameObjectSpawned(GameObjectSpawned event)
 	{
-		GameObject gameObject = event.getGameObject();
-		if (gameObject.getId() == 1204 && gameObject.getWorldLocation().getX() == 3815 && gameObject.getWorldLocation().getY() == 3024){
-			log.info("Plant found");
-			TroubleBrewingOverlay.renderableJunglePlant = gameObject;
-
-			int testvariable;
-			testvariable = gameObject.getId();
-			log.info("testvariable: "+ testvariable);
-			//scene location: 39, 16
+		final GameObject gameObject = event.getGameObject();
+		
+		if (gameObject.getId() == 1204                   &&
+		    gameObject.getWorldLocation().getX() == 3815 &&
+		    gameObject.getWorldLocation().getY() == 3024)
+		{
+			troubleBrewingOverlay.setRenderableJunglePlant(gameObject);
 		}
 	}
-
+	
 	@Subscribe
-	public void onGameObjectDespawned(GameObjectDespawned event)
+	public void
+	onGameObjectDespawned(GameObjectDespawned event)
 	{
-		log.info("GameObjectDespawned called");
-		GameObject gameObject = event.getGameObject();
-		if (gameObject.getId() == 1204 && gameObject.getWorldLocation().getX() == 3815 && gameObject.getWorldLocation().getY() == 3024){
-			log.info("Plant unfound");
-			TroubleBrewingOverlay.renderableJunglePlant = null;
-		} else {
-			TroubleBrewingOverlay.renderableJunglePlant = null;
+		final GameObject gameObject = event.getGameObject();
+		
+		if (gameObject.getId() == 1204                   &&
+		    gameObject.getWorldLocation().getX() == 3815 &&
+		    gameObject.getWorldLocation().getY() == 3024)
+		{
+			troubleBrewingOverlay.setRenderableJunglePlant(null);
 		}
 	}
-
-	@Subscribe
-	public void onGraphicsObjectCreated(GraphicsObjectCreated event)
-	{
-		log.info("GraphicsObjectCreated called");
-	}
-
+	
 	@Provides
-	TroubleBrewingConfig provideConfig(ConfigManager configManager)
+	TroubleBrewingConfig
+	provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(TroubleBrewingConfig.class);
 	}
 }
+
